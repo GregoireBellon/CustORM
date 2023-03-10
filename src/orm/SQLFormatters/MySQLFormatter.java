ab1ece81ccd3b8e92f8e402ad9cafdb6a6065106
Grégoire Bellon <gregoire.bellon@outlook.fr>
2023-03-10 14:55:15 +0100

    free ressources after

--- src/orm/SQLFormatters/MySQLFormatter.java
+++ src/orm/SQLFormatters/MySQLFormatter.java
@@ -20,23 +20,9 @@ import orm.selection.Selector;
 public class MySQLFormatter extends AbstractSQLFormatter {
 
 
-	private Connection con;
-
 	public MySQLFormatter(String db_url) throws SQLException  {
 		super(db_url);
 
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			this.con = DriverManager.getConnection(this.db_url);
-
-
-		} catch (ClassNotFoundException e) {
-
-			System.err.println("The mysql driver was not found, panicking");
-			e.printStackTrace();
-
-			System.exit(1);
-		}
 	}
 
 	@Override
@@ -67,10 +53,16 @@ public class MySQLFormatter extends AbstractSQLFormatter {
 
 		System.out.println("Final query : " + query);
 
-		try {
+		long ret = 0;
 
-			PreparedStatement preparedStmt;
-			preparedStmt = this.con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+		Connection con = null;
+		PreparedStatement preparedStmt = null;
+		
+		
+		try {
+			
+			con = this.getRawSQLConnection();
+			preparedStmt = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
 
 			for(int i = 0; i < fields.size(); i++) {
 				try {
@@ -89,19 +81,29 @@ public class MySQLFormatter extends AbstractSQLFormatter {
 
 			result.next();
 
-			long ret = result.getLong(1);
+			ret = result.getLong(1);
 
 			preparedStmt.close();
 
-			return ret;
-
 		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
+		}finally {
+			
+			try {
+				if(con!=null)
+					con.close();
+
+				if(preparedStmt!=null)
+					preparedStmt.close();
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 
 
-		return 0;
+
+		return ret;
 	}
 
 	@Override
@@ -115,9 +117,13 @@ public class MySQLFormatter extends AbstractSQLFormatter {
 
 		System.out.println("Final query : " + query);
 
-		PreparedStatement st;
+		Connection con = null;
+		PreparedStatement st = null;		
+		
 		try {
-			st = this.con.prepareStatement(query);
+			
+			con = this.getRawSQLConnection();
+			st = con.prepareStatement(query);
 
 			ResultSet rs = st.executeQuery();
 
@@ -132,6 +138,18 @@ public class MySQLFormatter extends AbstractSQLFormatter {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+			
+			try {
+				if(con!=null)
+					con.close();
+
+				if(st!=null)
+					st.close();
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 
 	}
@@ -142,18 +160,21 @@ public class MySQLFormatter extends AbstractSQLFormatter {
 			int limit) {
 
 		String query = "SELECT * FROM " + table_name + " ";
-
 		query += whereQuery(selectors);
-
 		query +=  (limit == -1)? ";" : "LIMIT " + limit + ";";
 
-		PreparedStatement st;
+		Connection con = null;
+		PreparedStatement st = null;
+		List<List<DataField>> ret = new ArrayList<List<DataField>>();
+
+		
 		try {
-			st = this.con.prepareStatement(query);
+			
+			con = this.getRawSQLConnection();
+			st = con.prepareStatement(query);
 
 			ResultSet rs = st.executeQuery();
 
-			List<List<DataField>> ret = new ArrayList<List<DataField>>();
 
 			while(rs.next()) {
 
@@ -174,15 +195,27 @@ public class MySQLFormatter extends AbstractSQLFormatter {
 
 			}
 
-			return ret;
-
-
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		finally {
+			
+			try {
 
-		return null;
+				if(con!=null)
+					con.close();
+
+				if(st!=null)
+					st.close();
+
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return ret;
 
 	}
 
@@ -211,184 +244,256 @@ public class MySQLFormatter extends AbstractSQLFormatter {
 
 		System.out.println("Final query : " + query);
 
-		try {
+		Connection con = null;
+		PreparedStatement preparedStmt = null;
 
-			PreparedStatement preparedStmt = this.con.prepareStatement(query);
+
+		try {
 			
+			con = this.getRawSQLConnection();
+			preparedStmt = con.prepareStatement(query);
+
 			for(int i = 0; i < fields.size(); i++) {
 				try {
-				
-				System.out.println("Adding the field " + fields.get(i).getName_in_db() + " with the value " + fields.get(i).getValue());
-				preparedStmt.setObject(i+1, fields.get(i).getValue());
+
+					System.out.println("Adding the field " + fields.get(i).getName_in_db() + " with the value " + fields.get(i).getValue());
+					preparedStmt.setObject(i+1, fields.get(i).getValue());
 				} catch (SQLException e) {
 					System.err.println("ORM : The " + fields.get(i).getName_in_db() + " type probably does not match with what has been provided.");
 					e.printStackTrace();
 				}
+			}
+
+			preparedStmt.execute();
+			preparedStmt.close();
+
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}finally {
+
+			try {
+
+				if(con!=null)
+					con.close();
+
+				if(preparedStmt!=null)
+					preparedStmt.close();
+
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			
 		}
-			
-		preparedStmt.execute();
-		preparedStmt.close();
 
-	} catch (SQLException e1) {
-		// TODO Auto-generated catch block
-		e1.printStackTrace();
-	}
-}
 
-@Override
-public int createTable(String table_name, List<DataField> fields) {
-
-	String query_content = "CREATE TABLE " + table_name + " (";
-
-	for(int i = 0; i < fields.size(); i++) {
-		DataField field = fields.get(i);
-
-		query_content += (i != 0 ? ", " : "");
-
-		query_content += this.fieldCreateLine(field);
 	}
 
-	List<DataField> foreign_keys = fields.stream().filter(f -> f.getForeign()!=null).toList();
+	@Override
+	public int createTable(String table_name, List<DataField> fields) {
 
-	for(int i = 0; i < foreign_keys.size(); i++) {
-		DataField field = foreign_keys.get(i);
+		String query_content = "CREATE TABLE " + table_name + " (";
+
+		for(int i = 0; i < fields.size(); i++) {
+			DataField field = fields.get(i);
+
+			query_content += (i != 0 ? ", " : "");
+
+			query_content += this.fieldCreateLine(field);
+		}
+
+		List<DataField> foreign_keys = fields.stream().filter(f -> f.getForeign()!=null).toList();
+
+		for(int i = 0; i < foreign_keys.size(); i++) {
+			DataField field = foreign_keys.get(i);
+
+			try {
+				query_content += ", ";
+
+				query_content += " FOREIGN KEY ("+field.getName_in_db() + ") REFERENCES " + field.getForeign().getForeign_class().getAnnotation(Table.class).name() + "(" +  field.getForeign().getForeign_attribute_name() + ")" ;
+
+			} catch (SecurityException e) {
+				System.err.println("The field " + field + " is badly associated with it's foreign key");
+			}
+		}
+
+
+
+		query_content += ");";
+
+		System.out.println("Generated query : " + query_content);
+
+		Connection con = null;
+		PreparedStatement s = null;
+
+		int ret = 0;
 
 		try {
-			query_content += ", ";
+			con = this.getRawSQLConnection();
+			s =  con.prepareStatement(query_content);
 
-			query_content += " FOREIGN KEY ("+field.getName_in_db() + ") REFERENCES " + field.getForeign().getForeign_class().getAnnotation(Table.class).name() + "(" +  field.getForeign().getForeign_attribute_name() + ")" ;
+			ret =  s.executeUpdate();
+			s.close();
 
-		} catch (SecurityException e) {
-			System.err.println("The field " + field + " is badly associated with it's foreign key");
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+
+			try {
+
+				if(con!=null)
+					con.close();
+
+				if(s!=null)
+					s.close();
+
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			
 		}
-	}
-
-
-
-	query_content += ");";
-
-	System.out.println("Generated query : " + query_content);
-
-	try {
-
-		PreparedStatement s =  this.con.prepareStatement(query_content);
-
-		int ret =  s.executeUpdate();
-		s.close();
 
 		return ret;
 
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
 	}
 
-	return 0;
-}
+	@Override
+	public int dropTable(String table_name) {
 
-@Override
-public int dropTable(String table_name) {
+		Connection con = null;
+		PreparedStatement s = null;
 
-	try {
+		try {
 
-		PreparedStatement s =  this.con.prepareStatement("DROP TABLE IF EXISTS " + table_name  +" ;");
-		//			s.setString(1, table_name);
+			con = this.getRawSQLConnection();
 
-		System.out.println("Request : "  + s.toString());
+			s =  con.prepareStatement("DROP TABLE IF EXISTS " + table_name  +" ;");
+			//			s.setString(1, table_name);
+
+			System.out.println("Request : "  + s.toString());
 
 
-		int ret = s.executeUpdate();
-		s.close();
-		return ret;
+			int ret = s.executeUpdate();
+			s.close();
+			return ret;
 
-	} catch (SQLException e) {
-		e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+
+			try {
+
+				if(con!=null)
+					con.close();
+
+				if(s!=null)
+					s.close();
+
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+
+		return 0;
 	}
 
-	return 0;
-}
 
+	private String fieldCreateLine(DataField field) {
 
-private String fieldCreateLine(DataField field) {
+		String line = field.getName_in_db() +  " " +  datatypeConversion(field.getType());
 
-	String line = field.getName_in_db() +  " " +  datatypeConversion(field.getType());
+		if(field.getConstraints().contains(Constraint.NOT_NULL)) {
+			line += " NOT NULL";
+		}
 
-	if(field.getConstraints().contains(Constraint.NOT_NULL)) {
-		line += " NOT NULL";
+		return line;
 	}
 
-	return line;
-}
+
+	@Override
+	public Connection getRawSQLConnection() {
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			return DriverManager.getConnection(this.db_url);
 
 
-@Override
-public Connection getRawSQLConnection() {
+		} catch (ClassNotFoundException | SQLException e) {
 
-	return this.con;
+			System.err.println("The mysql driver was not found, or the connexion didn't succeed");
+			e.printStackTrace();
 
-}
+			return null;
 
-public static String whereQuery(List<List<Selector>> selectors) {
+		}
 
-	String clause = "";
+	}
 
-	if(selectors != null && !selectors.isEmpty()) {
+	public static String whereQuery(List<List<Selector>> selectors) {
 
-		clause += "WHERE ";
+		String clause = "";
 
-		for(Iterator<List<Selector>> it_list = selectors.iterator(); it_list.hasNext();) {
-			List<Selector> li = it_list.next();
+		if(selectors != null && !selectors.isEmpty()) {
 
-			for(Iterator<Selector> it = li.iterator(); it.hasNext();) {
+			clause += "WHERE ";
 
-				Selector sel = it.next();
+			for(Iterator<List<Selector>> it_list = selectors.iterator(); it_list.hasNext();) {
+				List<Selector> li = it_list.next();
 
-				clause += sel.getA().getName_in_db() + " " + MySQLFormatter.comparatorConversion(sel.getComparator()) + " " + sel.getB()+ " ";
+				for(Iterator<Selector> it = li.iterator(); it.hasNext();) {
 
-				if(it.hasNext()) {
-					clause += "AND ";
+					Selector sel = it.next();
+
+					clause += sel.getA().getName_in_db() + " " + MySQLFormatter.comparatorConversion(sel.getComparator()) + " " + sel.getB()+ " ";
+
+					if(it.hasNext()) {
+						clause += "AND ";
+					}
 				}
-			}
 
-			if(it_list.hasNext()) {
-				clause += "OR " ;
-			}
+				if(it_list.hasNext()) {
+					clause += "OR " ;
+				}
 
+			}
+		}
+
+		return clause;
+
+	}
+
+
+	public static String datatypeConversion(DataTypes datatype) {
+		switch(datatype) {
+		case BIGINT : return "BIGINT";
+		case DATE : return "DATE";
+		case ID : return "SERIAL PRIMARY KEY";
+		case INT : return "INT";
+		case FOREIGN_ID : return "BIGINT UNSIGNED";
+		case BOOLEAN : return "BOOL";
+		case FLOAT : return "FLOAT";
+		case STRING : return "VARCHAR(50)";
+		case UUID : return "UUID";
+		case RAW : return "BLOB";
+		}
+		return null;
+	}
+
+	public static String comparatorConversion(Comparator comparator) {
+		switch(comparator) {
+		case DIFFERENT : return "!=";
+		case EQUALS : return "=";
+		case GREATER : return ">";
+		case GREATER_EQ : return ">=";
+		case LOWER : return "<";
+		case LOWER_EQ : return "<=";
+
+		default : return "=";
 		}
 	}
-
-	return clause;
-
-}
-
-
-public static String datatypeConversion(DataTypes datatype) {
-	switch(datatype) {
-	case BIGINT : return "BIGINT";
-	case DATE : return "DATE";
-	case ID : return "SERIAL PRIMARY KEY";
-	case INT : return "INT";
-	case FOREIGN_ID : return "BIGINT UNSIGNED";
-	case BOOLEAN : return "BOOL";
-	case FLOAT : return "FLOAT";
-	case STRING : return "VARCHAR(50)";
-	case UUID : return "UUID";
-	case RAW : return "BLOB";
-	}
-	return null;
-}
-
-public static String comparatorConversion(Comparator comparator) {
-	switch(comparator) {
-	case DIFFERENT : return "!=";
-	case EQUALS : return "=";
-	case GREATER : return ">";
-	case GREATER_EQ : return ">=";
-	case LOWER : return "<";
-	case LOWER_EQ : return "<=";
-
-	default : return "=";
-	}
-}
 
 }
